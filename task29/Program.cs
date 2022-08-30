ac6788df230c40b9ced80056ff3456bcdceecb72
1 1 <bobra2@ya.ru>
2022-08-30 09:01:55 +0300

    task 29 added

--- task29/Program.cs
+++ task29/Program.cs
@@ -3,4 +3,13 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 // 6, 1, 33 -> [6, 1, 33]
+Console.WriteLine("Please enter array size");
+int A = int.Parse(Console.ReadLine());
+int [] array=new int[8]; 
 
+
+for (int i=0;i<A;i++)
+{
+    array[i]=new Random().Next(0,100);
+    Console.Write($"{array[i]} ");
+}
