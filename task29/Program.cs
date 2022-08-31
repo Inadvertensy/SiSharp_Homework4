fc1d54846fd84bcb8e29a8446b87e2844a472d4a
1 1 <bobra2@ya.ru>
2022-08-31 08:21:28 +0300

    task 29 added

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,15 +1,31 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив 
-// из 8 элементов и выводит их на экран.
-
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+﻿// Напишите программу, которая задаёт массив из 
+// введенного числа и выводит их на экран.
+// пользователь вводит число элементов в массиве и 
+// минимальное, и максимальное значение . надо заполнить 
+// массив рандоными данными от минимального до максимального 
+// значение .И важное уточнениее , делать все в методе который возвращает массив
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
 Console.WriteLine("Please enter array size");
-int A = int.Parse(Console.ReadLine());
-int [] array=new int[8]; 
+int arraySize = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Please enter minimum value");
+int minValue = int.Parse(Console.ReadLine());
 
-for (int i=0;i<A;i++)
+Console.WriteLine("Please enter maximum value");
+int maxValue = int.Parse(Console.ReadLine());
+
+int[] array = new int[arraySize];
+
+void PrintArray(int[] array)
 {
-    array[i]=new Random().Next(0,100);
-    Console.Write($"{array[i]} ");
+    for (int i = 0; i < arraySize; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
+
+PrintArray(array);
